d896fe98d1c47f75826e2ecd6153c7fc1f42e276
GAAJINI <157499169+GAAJINI@users.noreply.github.com>
2024-03-12 09:52:44 -0600

    Add files via upload

---
+++ SEMANA 9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Gabriel Alejandro Ajin Izaguirre
+//1184924   
+//FIBONACCI
+Console.WriteLine("SEMAMA 9 - FIBONACCI");
+
+            Console.Write("Ingrese un numero N menor de 0 ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.WriteLine("Los primeros " + n + " términos de la sucesión de Fibonacci son:");
+            Console.Write(a + " " + b + " ");
+
+            int count = 2;
+            while (count < n)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+                count++;
+            }
+
+            Console.WriteLine();
